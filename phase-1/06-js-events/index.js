67e9c6663e79f81ac5b41a6904ce8a403e777aa9
Aysan Isayo <aisayo@icloud.com>
2021-09-02 18:15:37 -0700

    removed event listeners

--- phase-1/06-js-events/index.js
+++ phase-1/06-js-events/index.js
@@ -56,12 +56,10 @@ function renderPokemon(pokemon) {
   const likeBttn = document.createElement("button");
   likeBttn.className = "like-bttn";
   likeBttn.textContent = "♥";
-  likeBttn.addEventListener("click", () => increaseLike(pokemon, likesNum));
 
   const deleteBttn = document.createElement("button");
   deleteBttn.className = "delete-bttn";
   deleteBttn.textContent = "Delete";
-  deleteBttn.addEventListener("click", () => deletePoke(pokeCard));
 
   pokeCard.append(pokeImg, pokeName, pokeLikes, likesNum, likeBttn, deleteBttn);
   pokeContainer.appendChild(pokeCard);
