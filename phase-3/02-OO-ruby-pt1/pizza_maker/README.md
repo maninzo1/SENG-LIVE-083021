e8b0cf3e9d71bd7e42e19ebc0b65e720e7c53ef6
Aysan Isayo <aisayo@icloud.com>
2021-10-13 08:14:16 -0700

    breakout exercise

--- phase-3/02-OO-ruby-pt1/pizza_maker/README.md
+++ phase-3/02-OO-ruby-pt1/pizza_maker/README.md
@@ -2,10 +2,8 @@
 
 For this exercise, the goal is to create a Pizza class
 
-1. Create the pizza class and practice initializing new instances through binding.pry
-2. Add an 'initialize' method that sets the attributes: name, ingredients and desc. 
-3. In pry, create a new instance and try to read the attributes of that instance, starting with the name. (Write out the method, without using a macro)
-4. If this didn't work, think about what method could be missing to read the objects attributes (Write out the method, without using a macro)
-5. Refactor these two methods using a macro
-6. Now try to reassign the name of the pizza, whats missing here?
-7. Create an instance method called 'pizza_details' and print the objects attributes. Think about how 'self' can be used here.  
+1. Create a pizza class and practice initializing new instances through binding.pry
+2. Define an 'initialize' method that sets the attributes: name, ingredients and desc using instance variables. 
+3. While in pry, create a new pizza instance with a name, ingredients and desc attribute. Run the `.name` method on this new instance. What is the result? Discuss why this is happening and come up with how to solve the current result.(Write out the method, without using a macro)
+4. What type of method is this and why is it necessary? Now create a setter for the name attribute. 
+5. Writing a setter and getter method for each attribute can create a lot of code bloat. Let's refactor these two methods using a macro method that will produce both the setter and getter methods with 1 line of code. 
--- phase-3/02-OO-ruby-pt1/pizza_maker/lib/cli.rb
+++ phase-3/02-OO-ruby-pt1/pizza_maker/lib/cli.rb
@@ -1,35 +1,33 @@
-PIZZA = [] # why are we doing this?
-
+PIZZAS = [] 
 def init
   puts "Welcome to the lean, mean Pizza Machine App!"
   puts "Tell us your name: "
   user = gets.strip
   puts "Awesome, #{user.capitalize}, What would you like to do?"
-  menu
+  menu_options
   menu_selection
   goodbye
 end 
 
-def menu
-  puts "Enter the number of your selection, or 'exit' to leave the app."
-  menu_options
-end 
-
 def menu_options
+  puts "Enter the number of your selection, or 'exit' to leave the app."
   puts " 1. Create a new pizza!"
   puts " 2. View the list of created pizzas"
   puts " Change your mind? Type 'exit' to leave the app"
 end 
 
 def menu_selection
-  # Need to add some control flow based on users menu selection
-  while selection != 'exit'
-    if selection == "1"
-      puts "user selected 1"
+  selection = gets.strip
+  until selection == 'exit'
+    if selection == '1'
       create_pizza
-    elsif selection == "2"
-
+    elsif selection == '2'
+      created_pizzas
+    else 
+      puts "Invalid option. Try again: "
     end 
+    menu_options
+    selection = gets.strip
   end 
 end 
 
@@ -40,13 +38,31 @@ def create_pizza
   toppings = gets.strip
   puts "Describe this pizza: "
   desc = gets.strip 
+  pizza = {
+    name: name,
+    toppings: toppings,
+    desc: desc
+  }
 
-  # need to return a pizza hash
+  pizza_confirmation(pizza) 
+  PIZZAS << pizza 
+  pizza 
 end 
 
-# Currently there is not a method to print the list of created pizzas. In order to complete this:
-# 1. Think about a way to store and save the pizzas a user creates
-# 2. How can this collection be used to print a list of all of the stored pizzas
+def pizza_confirmation(pizza)
+  puts "The pizza you created is:"
+  puts "Name: #{pizza[:name]}"
+  puts "Toppings: #{pizza[:toppings]}"
+  puts "Desc: #{pizza[:desc]}"
+end 
+
+def created_pizzas
+  PIZZAS.each do |pizza| 
+    puts "Name: #{pizza[:name]}"
+    puts "Toppings: #{pizza[:toppings]}"
+    puts "Desc: #{pizza[:desc]}"
+  end 
+end 
 
 def goodbye
   puts "Come back when you get hungry!!!"
