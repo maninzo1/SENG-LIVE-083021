35670fef720722d99146d9b88628540c3df43fc4
Aysan Isayo <aisayo@icloud.com>
2021-10-15 14:31:56 -0700

    lecture code

--- phase-3/04_intro_to_databases/donation_tracker/config/environment.rb
+++ phase-3/04_intro_to_databases/donation_tracker/config/environment.rb
@@ -4,6 +4,7 @@ require "bundler/setup"
 Bundler.require(:default)
 # One of our dependencies that we loaded in the previous expression was the require_all gem. It allows us to require all of the files within a directory. The path we pass to it will be relative to the root path of the project (where the Gemfile is). In our case, we're loading all of the files inside of the lib directory, so if we write code in there (like a method) it will be accessible to us after the require_all below
 DB = SQLite3::Database.new("db/development.db")
+# DB = SQLite3::Database.new({conn: "db/development.db"})
 DB.results_as_hash = true
 require_all "lib"
 
--- phase-3/04_intro_to_databases/donation_tracker/lib/donation.rb
+++ phase-3/04_intro_to_databases/donation_tracker/lib/donation.rb
@@ -2,27 +2,41 @@ class Donation
 
     attr_accessor :amount, :date, :organization_id 
     attr_reader :id
+
+    # attr_accessor is creating these two methods:
+    # def amount 
+    #     @amount 
+    # end 
+
+    # def amount=(amount)
+    #     @amount = amount 
+    # end 
     
     def initialize(attributes) 
-        attributes.each do |key, value| 
-            if self.respond_to?("#{key.to_s}=")
-                self.send("#{key.to_s}=", value) 
+        attributes.each do |key, value| # malware
+            binding.pry
+            if self.respond_to?("#{key.to_s}=") # amount=
+                self.send("#{key.to_s}=", value) # this is invoking the setter method for the key
             end 
         end
+
     end
 
-    def save 
+    # 1st step: create a ruby object for donations
+    # 2nd step: save that ruby object as a DB record
+
+    def save # .save
         if self.id
             self.update
         else 
             sql = <<-SQL
-                INSERT INTO donations (amount, date, organization_id) VALUES (?, ?);
+                INSERT INTO donations (amount, date, organization_id) VALUES (?, ?, ?);
             SQL
 
             DB.execute(sql, self.amount, self.date, self.organization_id)
             @id = DB.last_insert_row_id
         end 
-        self 
+        self # instance 
     end
 
     def update 
@@ -36,7 +50,8 @@ class Donation
 
     def self.all 
         array_of_hashes = DB.execute("SELECT * FROM donations")
-        array_of_hashes.collect do |hash|
+        array_of_hashes.map do |hash|
+            binding.pry
           self.new(hash)
         end
     end
@@ -51,6 +66,18 @@ class Donation
         );
         SQL
         DB.execute(sql)
+        # DB[:conn].execute(sql)
+    end 
+
+    def name 
+        puts "testing send!!!"
     end 
 
 end 
+
+
+# invoke upon the initializ method
+donation = Donation.new(amount: 100, date: 101421, organization_id: 1)
+
+# save the ruby object as a db record
+donation.save
