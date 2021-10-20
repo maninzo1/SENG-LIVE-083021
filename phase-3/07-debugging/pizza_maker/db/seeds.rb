d2f6fef590286af2a1ef542370292e91943c3551
Aysan Isayo <aisayo@icloud.com>
2021-10-20 09:52:25 -0700

    starter code

--- phase-3/07-debugging/pizza_maker/db/seeds.rb
+++ phase-3/07-debugging/pizza_maker/db/seeds.rb
@@ -1,12 +1,16 @@
-# pizzas, ingredients and pizza_ingredients 
-
-
-# pizzas 
-
-pepperoni = Pizza.create(name: 'pepperoni pizza', desc: "yummy")
-pepperoni_ing = Ingredient.create(name: 'pepperoni')
+pepperoni = Ingredient.create(name: 'pepperoni')
 jalapeno = Ingredient.create(name: 'jalapeno')
+olives = Ingredient.create(name: 'olives')
+bacon = Ingredient.create(name: 'bacon')
+pineapple = Ingredient.create(name: 'pineapple')
+onions = Ingredient.create(name: 'onions')
+marinara = Ingredient.create(name: 'marinara')
+cheese = Ingredient.create(name: 'cheese')
 
+pepp_pizza = Pizza.create(name: 'Pepperoni pizza', desc: 'Marinara, cheese and pepperoni')
+pineapple_pizza = Pizza.create(name: 'Pineapple pizza', desc: 'Marinara, cheese, pineapple and pepperoni')
+bacon_pizza = Pizza.create(name: 'Bacon pizza', desc: 'Marinara, cheese, bacon, onions, and jalapeno')
 
-PizzaIngredient.create(pizza_id: pepperoni.id, ingredient_id: pepperoni_ing.id)
-# Pizza_Ingredient.create(pizza: pepperoni, ingredient: pepperoni_ing)
+PizzaIngredients.create(pizza_id: pepp_pizza.id, ingredient_id: pepperoni.id)
+PizzaIngredients.create(pizza_id: pepp_pizza.id, ingredient_id: marinara.id)
+PizzaIngredients.create(pizza_id: pepp_pizza.id, ingredient_id: cheese.id)
